0fedb5dfb20c6ea6ce70483cad925700195d51de
A. Matías Quezada <amatiasq@gmail.com>
2013-08-07 15:17:01 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -1,38 +1,7 @@
-angular.module('lorelei-drive', [])
-
-
-.directive('folder', function() {
-	return {
-		restrict: 'A',
-		template: '<button>CLICKME</button>',
-		link: function link(scope, element, attributes) {
-			console.log('running folder');
-		},
-		compile: function compile(tElement, tAttrs) {
-			console.log('compiling folder');
-			return function postLink(scope, iElement, iAttrs) {
-				console.log('postlink folder');
-			}
-		}
-	};
-})
-
-
-.directive('file', function() {
-	return {
-		restrict: 'E',
-		template: '<button>CLICKME</button>',
-		link: function link(scope, element, attributes) {
-			console.log('running file');
-		},
-		compile: function compile(tElement, tAttrs) {
-			console.log('compiling file');
-			return function postLink(scope, iElement, iAttrs) {
-				console.log('postlink file');
-			}
-		}
-	};
-})
+angular.module('lorelei-drive', [
+	'lorelei-drive-folder',
+	'lorelei-drive-file',
+])
 
 
 .controller('MasterCtrl', function($scope) {
